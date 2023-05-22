38383bcca2a1b6cca8cdc022564470a30a4efeaa
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-05-22 16:09:15 +0545

    .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout-then-login/', views.logout_then_login, name='logout_then_login'),
+    # path('account/', views.account_view, name='account'),
+
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+def logout_then_login(request):
+    logout(request)
+    return(redirect('login'))
 
 def user_login(request):
     if request.method == 'POST':
@@ -23,3 +28,9 @@ def user_login(request):
         form = LoginForm()
     
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section':'dashboard'})
