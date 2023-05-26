1f612deb5bc217ab31be154cddc2efc23c3335d8
DeepakRanaMagar <dpk.me.123@gmail.com>
2023-05-26 08:41:05 +0545

    .

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout-then-login/', views.logout_then_login, name='logout_then_login'),
     path('dashboard/',views.dashboard, name='dashboard'),
+    path('password-change/',auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
